e4e4988213227659a039f3e1ea6fed073d56919d
Pa1nkil3er <kiril.july@gmail.com>
2022-11-01 23:58:56 +0200

    update

--- Homework 8/Завдання 2/JS/script3.js
+++
@@ -1,6 +0,0 @@
-let day = +prompt("Введіть день: ");
-let month = +prompt("Введіть місяць: ");
-let year = +prompt("Введіть рік: ");
-let date = new Date(`${year}-${month}-${day}`);
-date.setDate(data.getDate() + 1);
-console.log(`Наступна дата : ${date.toLocaleDateString()}`);
