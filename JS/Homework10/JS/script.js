4efe43cd6be6744aa0b3cd72be2ce9f0079bca15
Pa1nkil3er <kiril.july@gmail.com>
2022-11-22 00:12:42 +0200

    update

--- JS/Homework10/JS/script.js
+++ JS/Homework10/JS/script.js
@@ -71,20 +71,46 @@ clearBtn.onclick = () => {
 	table.innerHTML = "";
 }
 function Search() {
+	let found;
 	let searchInput = document.getElementById('searchInput');
 	let filter = searchInput.value.toUpperCase();
 	let tr = table.getElementsByTagName("tr");
-	for (let index = 0; index < tr.length; index++) {
-		let td = table.getElementsByTagName("td")[0];
-		let txtValue = td.textContent || a.innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+	for (let i = 0; i < tr.length; i++) {
+		let td = tr[i].getElementsByTagName("td");
+		for (let j = 0; j < td.length; j++) {
+			if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+				found = true;
+			}
+		}
+		if (found) {
 			tr[i].style.display = "";
+			found = false;
 		}
 		else {
-			tr.style.display = "none";
+			tr[i].style.display = "none";
 		}
 
 	}
 }
 
-
+// function searchTable() {
+// 	var input, filter, found, table, tr, td, i, j;
+// 	input = document.getElementById("myInput");
+// 	filter = input.value.toUpperCase();
+// 	table = document.getElementById("myTable");
+// 	tr = table.getElementsByTagName("tr");
+// 	for (i = 0; i < tr.length; i++) {
+// 		td = tr[i].getElementsByTagName("td");
+// 		for (j = 0; j < td.length; j++) {
+// 			if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+// 				found = true;
+// 			}
+// 		}
+// 		if (found) {
+// 			tr[i].style.display = "";
+// 			found = false;
+// 		} else {
+// 			tr[i].style.display = "none";
+// 		}
+// 	}
+// }
