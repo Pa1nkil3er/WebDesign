83459540434573241a67093a62b665e2e5ec23f1
Pa1nkil3er <kiril.july@gmail.com>
2022-12-06 20:53:58 +0200

    done

--- JS/Homework12/JS/script.js
+++ JS/Homework12/JS/script.js
@@ -11,11 +11,15 @@ addBtn.onclick = () => {
 	inputNumber = Num.value;
 	inputMass = Mass.value;
 	if (Symbol.value != "" && Name.value != "" && Num.value != "" && Mass.value != "") {
-		table.innerHTML += `<tr id="new_tr"><td>${inputSymbol}</td><td>${inputName}</td><td>${inputNumber}</td><td>${inputMass}</td><td><button id="clearTr">Clear</button></td></tr> `;
+		table.innerHTML += `<tr><td>${inputSymbol}</td><td>${inputName}</td><td>${inputNumber}</td><td>${inputMass}</td><td><button id="clearTr" onclick="ClearTr(this)">Clear</button></td></tr> `;
 	}
 }
 clearBtn.onclick = () => {
 	table.innerHTML = "";
+}
+function ClearTr(r) {
+	let i = r.parentNode.parentNode.rowIndex;
+	table.deleteRow(i);
 }
 
 function Search() {
